3d02d63aa4dec4b9a766dcff7253fc50f4283dad
Paolo1698 <jorge.arellano@ucr.ac.cr>
2026-09-15 11:52:32 -0600

    feat:bootstrap springboot project with gradle and java 26

---
+++ src/main/java/com/example/springBootIA/SpringBootIaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBootIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springBootIA/SpringBootIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBootIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
